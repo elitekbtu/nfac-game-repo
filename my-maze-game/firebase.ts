a3ee45405182674bca56fddc5460b0297f02f833
elitekbtu <satbaldievst@mail.ru>
2025-07-17 14:59:20 +0500

    fiebase config

---
+++ my-maze-game/firebase.ts
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBJ4ipSdlq_SYjZQSuBy2nOgbFJbTis388",
+  authDomain: "nfacoiyn.firebaseapp.com",
+  projectId: "nfacoiyn",
+  storageBucket: "nfacoiyn.firebasestorage.app",
+  messagingSenderId: "275843226256",
+  appId: "1:275843226256:web:0edf273bca9f55453a9615"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app; 
